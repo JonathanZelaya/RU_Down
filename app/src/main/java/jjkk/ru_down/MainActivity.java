1699573c0bb35a0fe1ec08c82346bbe0cd70b1d1
Jonathan Zelaya <zelaya0805@gmail.com>
2017-04-23 04:42:23 -0400

    removed most warnings

--- app/src/main/java/jjkk/ru_down/MainActivity.java
+++ app/src/main/java/jjkk/ru_down/MainActivity.java
@@ -1,21 +1,32 @@
 package jjkk.ru_down;
 
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import static android.R.attr.button;
+import static android.R.attr.data;
+import static android.R.attr.password;
 
 public class MainActivity extends AppCompatActivity {
     private DatabaseReference mDatabase;
@@ -24,6 +35,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
 
         mDatabase = FirebaseDatabase.getInstance().getReference();
         //writeNewUser("Jonathan Zelaya", "zelaya0805@gmail.com", "password");
@@ -35,9 +49,34 @@ public class MainActivity extends AppCompatActivity {
                 .setPositiveButton("Login", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
-                        if(userValid()){
-                            // close dialog and store values in SharedPreferences
+                        EditText emailET = (EditText) view.findViewById(R.id.et_email);
+                        final String email = emailET.getText().toString();
 
+                        EditText passwordET = (EditText) view.findViewById(R.id.et_password);
+                        String password = passwordET.getText().toString();
+
+                        if(userValid(email, password)){
+                            // close dialog and store values in SharedPreferences
+                            DatabaseReference ref = mDatabase.child("users").child(email);
+                            ref.addListenerForSingleValueEvent(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(DataSnapshot dataSnapshot) {
+                                    Context context = MainActivity.this;
+                                    SharedPreferences sharedPref = getSharedPreferences("sharedPref", context.MODE_PRIVATE);
+                                    SharedPreferences.Editor editor = sharedPref.edit();
+
+                                    editor.putString("campus",dataSnapshot.child("preferredCampus").getValue().toString());
+                                    editor.putString("gender",dataSnapshot.child("preferredGender").getValue().toString());
+                                    editor.putString("name",dataSnapshot.child("fullName").getValue().toString());
+                                    editor.putString("email",email);
+                                    editor.commit();
+                                }
+
+                                @Override
+                                public void onCancelled(DatabaseError databaseError) {
+
+                                }
+                            });
                         }else{
                             Toast.makeText(MainActivity.this, "Invalid login, try again.", Toast.LENGTH_SHORT).show();
                         }
@@ -54,13 +93,20 @@ public class MainActivity extends AppCompatActivity {
                         String password = passwordET.getText().toString();
 
                         writeNewUser("User",email,password);
-                        storeUserInfo();
+                        storeUserInfo(email);
                     }
                 })
                 .setCancelable(false);
 
         AlertDialog alert = builder.create();
         alert.show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater(); // reads XML
+        inflater.inflate(R.menu.menu, menu); // to create
+        return super.onCreateOptionsMenu(menu); // the menu
     }
 
     public void goToSports(View view){
@@ -75,11 +121,59 @@ public class MainActivity extends AppCompatActivity {
         mDatabase.child("users").child(email).setValue(user);
     }
 
-    private void storeUserInfo(){
+    private void storeUserInfo(String email){
+        Context context = MainActivity.this;
+        SharedPreferences sharedPref = getSharedPreferences("sharedPref", context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+
+        email = email.replace(".","dot");
+        editor.putString("email", email);
+        editor.commit();
+
         //mDatabase.addListenerForSingleValueEvent();
     }
 
-    private boolean userValid(){
-        return false;
+    private boolean userValid(String email, final String password){
+        DatabaseReference ref = mDatabase.child("users").child(email);
+        boolean isValid = false;
+
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Context context = MainActivity.this;
+                //System.out.println("pass is: " + dataSnapshot.child("password").getValue().toString());
+                boolean check = dataSnapshot.child("password").getValue().toString().equals(password);
+                SharedPreferences sharedPref = getSharedPreferences("sharedPref", context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putBoolean("validLogin",check);
+                editor.commit();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // ...
+            }
+        });
+
+        Context context = MainActivity.this;
+        SharedPreferences sharedPref = getSharedPreferences("sharedPref", context.MODE_PRIVATE);
+        isValid = sharedPref.getBoolean("validLogin",false);
+        return isValid;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Intent i = new Intent(this, SettingsActivity.class);
+                startActivity(i);
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
---
+++ app/src/main/java/jjkk/ru_down/SettingsActivity.java
@@ -0,0 +1,82 @@
+package jjkk.ru_down;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    private DatabaseReference mDatabase;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        SharedPreferences sharedPref = getSharedPreferences("sharedPref", SettingsActivity.this.MODE_PRIVATE);
+        String campus = sharedPref.getString("campus",null);
+        String gender = sharedPref.getString("gender",null);
+        String name = sharedPref.getString("name",null);
+
+        Spinner campusSpinner = (Spinner) findViewById(R.id.spin);
+        Spinner genderSpinner = (Spinner) findViewById(R.id.spinner2);
+        EditText displayName = (EditText) findViewById(R.id.editText);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.Campus, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        campusSpinner.setAdapter(adapter);
+        if (!campus.equals(null)) {
+            int spinnerPosition = adapter.getPosition(campus);
+            campusSpinner.setSelection(spinnerPosition);
+        }
+
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.Gender, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        genderSpinner.setAdapter(adapter2);
+        if (!gender.equals(null)) {
+            int spinnerPosition = adapter2.getPosition(gender);
+            genderSpinner.setSelection(spinnerPosition);
+        }
+
+        displayName.setText(name);
+
+
+
+
+    }
+
+    public void save(View view){
+        Context context = SettingsActivity.this;
+        SharedPreferences sharedPref = context.getSharedPreferences("sharedPref", context.MODE_PRIVATE);
+        String email = sharedPref.getString("email","Not found");
+
+        Spinner campus = (Spinner) findViewById(R.id.spin);
+        Spinner gender = (Spinner) findViewById(R.id.spinner2);
+        EditText displayName = (EditText) findViewById(R.id.editText);
+
+        String campusString = campus.getSelectedItem().toString();
+        String genderString = gender.getSelectedItem().toString();
+        String name = displayName.getText().toString();
+
+        mDatabase.child("users").child(email).child("preferredCampus").setValue(campusString);
+        mDatabase.child("users").child(email).child("preferredGender").setValue(genderString);
+        mDatabase.child("users").child(email).child("fullName").setValue(name);
+
+        SharedPreferences.Editor editor = sharedPref.edit();
+
+        editor.putString("campus",campusString);
+        editor.putString("gender",genderString);
+        editor.putString("name",name);
+        editor.commit();
+
+        finish();
+
+    }
+}
